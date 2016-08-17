dc38648ee37e48a044fb317d74da7d4926715e93
Tania <tatashat@gmail.com>
2016-08-17 19:09:54 +0000

    + ip address, language

--- server.js
+++ server.js
@@ -2,7 +2,14 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  var ip = req.get('x-forwarded-for');
+  var language = req.acceptsLanguages()[0];
+  
+  res.json({
+    "ipaddress": ip,
+    "language": language,
+    "software": req.headers['user-agent']
+  });
 });
 
 app.listen(8080, function () {
