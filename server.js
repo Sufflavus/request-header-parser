7d7e872d126e8817d81e41bd1fdb581ef2ba1769
Tania <tatashat@gmail.com>
2016-08-18 20:50:07 +0000

    * os]

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 var express = require('express');
+
 var app = express();
 
 app.get('/', function (req, res) {
   var ip = req.get('x-forwarded-for');
   var language = req.acceptsLanguages()[0];
+  var agent = req.headers['user-agent'];
+  var rex = /\((.*?)\)/g;
+  var maches = rex.exec(agent);
+  var os = maches[0].split('(')[1].split(')')[0];
   
   res.json({
     "ipaddress": ip,
     "language": language,
-    "software": req.headers['user-agent']
+    "software": os
   });
 });
 
